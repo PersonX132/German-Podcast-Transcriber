4b29746800ed8c0702984875f3172be463c7562e
Manthan Patki <88187801+PersonX132@users.noreply.github.com>
2025-07-08 19:44:07 +0530

    Created __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+import os
+import click
+from flask import Flask
+from flask.cli import with_appcontext
+
+from .models import db
+from .routes import main_routes
+
+def create_app():
+    """creates and configures Flask instance"""
+    app = Flask(__name__, instance_relative_config=True)
+  
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    
+    secret_key = os.environ.get('SECRET_KEY', 'fallback') # Load from environment variables, uses fallback as a fallback
+
+    app.config.from_mapping(
+        SECRET_KEY=secret_key,
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.join(basedir, 'library.db')}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        UPLOAD_FOLDER=os.path.join(basedir, 'temp_uploads'),
+        AUDIO_LIBRARY_FOLDER=os.path.join(basedir, 'audio_library')
+    )
+
+    try:
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+        os.makedirs(app.config['AUDIO_LIBRARY_FOLDER'], exist_ok=True)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    app.register_blueprint(main_routes)
+
+    @click.command('init-db')
+    @with_appcontext
+    def init_db_command():
+        """Clear existing data and create new tables."""
+        db.create_all()
+        click.echo('Initialized the database.')
+    
+    app.cli.add_command(init_db_command)
+
+    return app
